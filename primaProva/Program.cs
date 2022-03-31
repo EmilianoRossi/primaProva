5b362456f1648a4c880a8799ca3c343c001928ed
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-03-31 11:55:39 +0200

    Add project files.

---
+++ primaProva/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("aggiungi un nome: ");
+
+var nomeUtente = Console.ReadLine();
+
+Console.WriteLine(nomeUtente);
